ed80d56c562c8a96edd72988be37f936bfa0b08e
Lucas Ramos <lrsantostw@gmail.com>
2021-10-12 01:35:00 -0300

    Add show password button

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import ErrorMessage from './ErrorMessage'
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai'
 
 interface Props {
   className?:string,
@@ -10,14 +11,29 @@ interface Props {
   valueError?: string
 }
 
-const Input: FC<Props> = ({ valueError, className, ...rest }) => {
+const Input: FC<Props> = ({ valueError, className, type, ...rest }) => {
+  const [passVisible, setPassVisible] = useState(false)
+
   let classes = className || ''
   if (valueError) {
     classes += ' error-input'
   }
+
+  const isPassword = type === 'password'
+  let passedType = type || ''
+  if (isPassword && passVisible) {
+    passedType = 'text'
+  }
   return (
     <div className="input-container">
-      <input className={classes} {...rest}/>
+      <div className="row">
+        <input className={classes} type={passedType} {...rest}/>
+        {isPassword &&
+          <div className="row" onClick={() => setPassVisible(!passVisible)}>
+            {!passVisible ? <AiOutlineEye className="eye-button"/> : <AiOutlineEyeInvisible className="eye-button"/>}
+          </div>
+        }
+      </div>
       {
         !!valueError && <ErrorMessage message={valueError}/>
       }
